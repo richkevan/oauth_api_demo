bf7d4dcf796fec2d1dd8725008896f1e76a3f2ad
Rich Kevan <rskevan@me.com>
2023-10-17 21:41:02 -0700

    Static files attempt 2

--- admin/settings.py
+++ admin/settings.py
@@ -136,8 +136,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
-STATIC_ROOT = 'https://oauth-api-demo.onrender.com/'
+STATIC_URL = '/static/'
+STATIC_ROOT = '/opt/render/project/src'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
