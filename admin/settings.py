ed2a69064b9874817a8fc1ad7080f571c9eb024c
Rich Kevan <rskevan@me.com>
2023-10-17 23:20:45 -0700

    update to database settings

--- admin/settings.py
+++ admin/settings.py
@@ -86,18 +86,19 @@ WSGI_APPLICATION = 'admin.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
-if not DEBUG:
-    DATABASES = {'default': dj_database_url.config(
-        default=env("DATABASE_URL"),
-        conn_max_age=600,
-                    )}
-else:
+if DEBUG:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.sqlite3',
             'NAME': BASE_DIR / 'db.sqlite3',
         }
     }
+else:
+    DATABASES = {'default': dj_database_url.config(
+        default=env("DATABASE_URL"),
+        conn_max_age=600,
+                    )}
+    
 
 
 # Password validation
